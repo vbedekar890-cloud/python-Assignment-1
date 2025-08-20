c064de7646173b06c5bf0b8b57d1303ca13aa8ec
vbedekar890-cloud <vbedekar890@gmail.com>
2025-08-20 17:50:44 +0530

    Add files via upload

---
+++ Task No.2.py
@@ -0,0 +1,6 @@
+from queue import Full
+
+first_Name = input("Enter First Name: ")
+second_Name = input("Enter Second Name: ")
+Full_Name = first_Name + " " + second_Name
+print("Hello, " + Full_Name + ", Welcome to Python program. ")
