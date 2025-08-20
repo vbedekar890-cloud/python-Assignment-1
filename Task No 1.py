9e058089d867afaf3d2c331d3b235d2c5ce3b631
vbedekar890-cloud <vbedekar890@gmail.com>
2025-08-20 17:50:20 +0530

    Add files via upload

---
+++ Task No 1.py
@@ -0,0 +1,11 @@
+a=float(input("Enter a  first number(a) :  "))
+b= float (input("Enter Second Number (b) :  "))
+Add = a+b
+Sub = a-b
+Mult = a*b
+Div = a/b
+
+print("Addition =", Add)
+print("Subtraction =", Sub)
+print("Multiplication =", Mult)
+print("Division =", Div)
